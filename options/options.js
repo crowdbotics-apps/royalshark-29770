41f19158f1608b4437c22f9d69ec7d14ebaa2980
Crowdbotics <team@crowdbotics.com>
2021-08-12 21:12:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "royalshark_29770Identifier",
+  url: "https://royalshark_29770Identifier.botics.co",
+  api: "https://royalshark_29770Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
