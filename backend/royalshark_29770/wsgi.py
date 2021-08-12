f8697ac7aac0e4e2dff6f90ae20212d73b60e038
Crowdbotics <team@crowdbotics.com>
2021-08-12 21:13:09 +0000

    Update models

--- backend/royalshark_29770/wsgi.py
+++ backend/royalshark_29770/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'royalshark_29770.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "royalshark_29770.settings")
 
 application = get_wsgi_application()
